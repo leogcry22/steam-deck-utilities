9edd31615f8acb4dd70c5c18939d1e01a2c7a0e1
22 <leogcry22@users.noreply.github.com>
2023-07-25 10:47:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Author: CryoByte33
+# Author: leogcry22
 
 # Create a hidden directory for the script, if not present
 mkdir -p "$HOME/.cryo_utilities" &>/dev/null
 cd "$HOME/.cryo_utilities" || exit 1
 
 # Download checksum to compare with local binary, if present
-wget https://github.com/CryoByte33/steam-deck-utilities/releases/download/latest/cu.md5 -O "$HOME/.cryo_utilities/cu.md5" 2>&1
+wget https://ghproxy.com/https://github.com/leogcry22/steam-deck-utilities/releases/download/latest/cu.md5 -O "$HOME/.cryo_utilities/cu.md5" 2>&1
 sleep 1
 if md5sum -c --quiet cu.md5; then
   zenity --info --text="No update necessary!" --width=300
@@ -27,7 +27,7 @@ rm -f "$HOME/.cryo_utilities/cryo_utilities" &>/dev/null
 # Attempt to download the binary 3 times.
 for i in {1..3}; do
   # Download binary
-  wget https://github.com/CryoByte33/steam-deck-utilities/releases/download/latest/cryo_utilities -O "$HOME/.cryo_utilities/cryo_utilities" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading CU Binary, attempt $i of 3..." --auto-close --width=500
+  wget https://ghproxy.com/https://github.com/leogcry22/steam-deck-utilities/releases/download/latest/cryo_utilities -O "$HOME/.cryo_utilities/cryo_utilities" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading CU Binary, attempt $i of 3..." --auto-close --width=500
 
   # Start a loop testing if zenity is running, and if not kill wget (allows for cancel to work)
   RUNNING=0
@@ -58,14 +58,14 @@ rm -f cu.md5 &>/dev/null
 rm -f "$HOME/.cryo_utilities/launcher.sh" &>/dev/null
 
 # Install launcher script
-wget https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/launcher.sh -O "$HOME/.cryo_utilities/launcher.sh"
+wget https://ghproxy.com/https://raw.githubusercontent.com/leogcry22/steam-deck-utilities/main/launcher.sh -O "$HOME/.cryo_utilities/launcher.sh"
 chmod +x "$HOME/.cryo_utilities/launcher.sh"
 
 # Remove old icon
 rm -f "$HOME/.cryo_utilities/cryo-utilities.png" &>/dev/null
 
 # Install Icon
-wget https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/cmd/cryoutilities/Icon.png -O "$HOME/.cryo_utilities/cryo-utilities.png"
+wget https://ghproxy.com/https://raw.githubusercontent.com/leogcry22/steam-deck-utilities/main/cmd/cryoutilities/Icon.png -O "$HOME/.cryo_utilities/cryo-utilities.png"
 xdg-icon-resource install cryo-utilities.png --size 64
 
 # Create Desktop icons
@@ -73,7 +73,7 @@ rm -rf "$HOME"/Desktop/CryoUtilitiesUninstall.desktop 2>/dev/null
 echo '#!/usr/bin/env xdg-open
 [Desktop Entry]
 Name=Uninstall CryoUtilities
-Exec=curl https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/uninstall.sh | bash -s --
+Exec=curl https://ghproxy.com/https://raw.githubusercontent.com/leogcry22/steam-deck-utilities/main/uninstall.sh | bash -s --
 Icon=delete
 Terminal=false
 Type=Application
@@ -95,7 +95,7 @@ rm -rf "$HOME"/Desktop/UpdateCryoUtilities.desktop 2>/dev/null
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Name=Update CryoUtilities
-Exec=curl https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/install.sh | bash -s --
+Exec=curl https://ghproxy.com/https://raw.githubusercontent.com/leogcry22/steam-deck-utilities/main/install.sh | bash -s --
 Icon=bittorrent-sync
 Terminal=false
 Type=Application
@@ -107,7 +107,7 @@ rm -rf "$HOME"/.local/share/applications/CryoUtilitiesUninstall.desktop 2>/dev/n
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Name=CryoUtilities - Uninstall
-Exec=curl https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/uninstall.sh | bash -s --
+Exec=curl https://ghproxy.com/https://raw.githubusercontent.com/leogcry22/steam-deck-utilities/main/uninstall.sh | bash -s --
 Icon=delete
 Terminal=false
 Type=Application
@@ -131,7 +131,7 @@ rm -rf "$HOME"/.local/share/applications/UpdateCryoUtilities.desktop 2>/dev/null
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Name=CryoUtilities - Update
-Exec=curl https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/install.sh | bash -s --
+Exec=curl https://ghproxy.com/https://raw.githubusercontent.com/leogcry22/steam-deck-utilities/main/install.sh | bash -s --
 Icon=bittorrent-sync
 Terminal=false
 Type=Application
